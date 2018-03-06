9fef972b96d92f4973ccfedcf2018383f2cc6521
Fernando Moreira <nandomoreira.me@gmail.com>
2018-03-05 22:32:30 -0300

    Fix twitter widget

--- inc/twitter-iframe.php
+++ inc/twitter-iframe.php
@@ -1,9 +1,11 @@
 <h3 class="section__title section__title--cast">
   <a href="https://twitter.com/phppr_org?ref_src=twsrc%5Etfw" target="_blank">
-    <i class="icon icon-twitter" aria-hidden="true"></i> No twitter
+    <i class="icon icon-twitter" aria-hidden="true"></i> Siga PHPPR no twitter
   </a>
 </h3>
 <div class="card card--twitter">
-  <a class="twitter-timeline" href="https://twitter.com/phppr_org?ref_src=twsrc%5Etfw">Tweets by phppr_org</a>
-  <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+  <div class="card__inner">
+    <a class="twitter-timeline" href="https://twitter.com/phppr_org?ref_src=twsrc%5Etfw">Tweets by phppr_org</a>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+  </div>
 </div>
